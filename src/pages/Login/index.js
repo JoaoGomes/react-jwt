c0fd064b5c0ed258ddced41ca10f48d490f383ff
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-08 11:03:28 -0300

    Acréscimo de arquivos.

---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+	function handleLogin() {}
+	return (
+		<div>
+		    <button onClick={handleLogin}>Login</button>
+		</div>
+	);
+};
+
+export default Login;
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignRoutes from './SignRoutes';
+
+const Routes =() => {
+	return <SignRoutes />;
+};
+
+export default Routes;
